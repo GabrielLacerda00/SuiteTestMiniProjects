13bb19d96880c6ca77c69125a47b2e9679fb3434
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-09-02 18:33:37 -0300

    code after refactoring:rename method

--- OneRenameForTwoCaller/Calculator/src/main/java/org/example/code/Calculator.java
+++ OneRenameForTwoCaller/Calculator/src/main/java/org/example/code/Calculator.java
@@ -2,7 +2,7 @@ package Calculator.src.main.java.org.example.code;
 
 public class Calculator {
 
-        public static int add(int a, int b) {
+        public static int sum(int a, int b) {
             return a + b;
         }
 
@@ -24,8 +24,8 @@ public class Calculator {
 
 
         public static void main (String args[]) {
-            System.out.println(add(1,2));
-            System.out.println(add(1,2));
+            System.out.println(sum(1,2));
+            System.out.println(sum(1,2));
         }
 
 }
