e9432b2a8a72795bfa59b73e855bb49e41e95023
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-09-03 11:28:03 -0300

    code before refactoring: rename method

---
+++ OneRenameForThreCallersInOtherClasses/CodeDestiny/Calculator/src/main/java/org/example/p1/Calculator.java
@@ -0,0 +1,29 @@
+package org.example.p1;
+
+
+public class Calculator {
+	
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public float division(int a, int b) {
+		return a / b;
+	}
+	
+	public MyObj testComplex(MyObj myobj) {
+		return myobj;
+	}
+
+	public static void main (String args[]) {
+
+	}
+}
