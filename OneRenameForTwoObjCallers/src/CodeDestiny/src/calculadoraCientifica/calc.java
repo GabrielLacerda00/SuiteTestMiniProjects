c85eea9cfbfcc9391b35c69e53f751d00bd7adeb
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-09-03 20:48:17 -0300

    salving projects

--- OneRenameForTwoObjCallers/src/CodeDestiny/src/calculadoraCientifica/calc.java
+++
@@ -1,9 +0,0 @@
-public class calc {
-  public static void main(String[] args) {
-
-  }
-  
-  public double sum() {
-      return 1.0 + 2.0;
-  }
-}
--- OneRenameForTwoObjCallers/src/CodeOrigin/src/calculadoraCientifica/calc.java
+++
@@ -1,9 +0,0 @@
-public class calc {
-  public static void main(String[] args) {
-
-  }
-  
-  public double soma() {
-      return 1.0 + 2.0;
-  }
-}
