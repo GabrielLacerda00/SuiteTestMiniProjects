e34eb678dc8e30a4e2d0078cdf982e07f21703fd
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-09-03 10:23:00 -0300

    code before refactoring

---
+++ OneRenameForOneCallerInOtherClasse/CodeDestiny/Calculator/src/main/java/org/example/p1/MyObj.java
@@ -0,0 +1,20 @@
+package org.example.p1;
+
+public class MyObj extends Calculator {
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+
+    public static void main (String args[]) {
+        sum(1,2);
+    }
+}
+
+
+
---
+++ OneRenameForOneCallerInOtherClasse/CodeOrigin/Calculator/src/main/java/org/example/p1/MyObj.java
@@ -0,0 +1,20 @@
+package org.example.p1;
+
+public class MyObj extends Calculator {
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+
+    public static void main (String args[]) {
+        add(1,2);
+    }
+}
+
+
+
