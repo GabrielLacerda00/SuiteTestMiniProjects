b75d7bbf87ecfc72a9bb7b802e35565daf089b8d
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-09-03 16:53:08 -0300

    code before refactoring

---
+++ OneRenameForThreObjCallersInOtherClasses/CodeOrigin/Calculator/src/main/java/org/example/p1/Calculator.java
@@ -0,0 +1,27 @@
+package org.example.p1;
+
+
+public class Calculator {
+	
+	public  int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public float division(int a, int b) {
+		return a / b;
+	}
+
+
+	public static void main (String args[]) {
+		Calculator cal = new Calculator();
+		cal.add(5,6);
+	}
+}
