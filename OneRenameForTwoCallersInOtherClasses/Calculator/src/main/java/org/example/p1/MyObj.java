262d6d057ff40662653b86eec45f1832843e73c3
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-09-03 10:54:44 -0300

    code before refactoring: rename method

---
+++ OneRenameForTwoCallersInOtherClasses/Calculator/src/main/java/org/example/p1/MyObj.java
@@ -0,0 +1,21 @@
+package org.example.p1;
+
+public class MyObj  extends Calculator{
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+
+    public static void main (String args[]) {
+        add(4,5);
+        add(6,8);
+    }
+}
+
+
+
