be0962534f1a7af26e2a1fec3e0f79653b3bb463
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-09-03 20:53:20 -0300

    salving projectss

---
+++ OneRenameForTwoObjCallersInOtherClasses/CodeOrigin/Calculator/src/main/java/org/example/p1/MyObj.java
@@ -0,0 +1,22 @@
+package org.example.p1;
+
+public class MyObj  extends Calculator{
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+
+    public static void main (String args[]) {
+        Calculator cal = new Calculator();
+        cal.add(4,5);
+        cal.add(6,8);
+    }
+}
+
+
+
