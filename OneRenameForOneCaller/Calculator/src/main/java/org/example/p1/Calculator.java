798608b6e62d06e05a10f56b3e3fa22a0f3b615f
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-09-04 00:15:47 -0300

    OneRenameForOneCaller: code before refactoring

---
+++ OneRenameForOneCaller/Calculator/src/main/java/org/example/p1/Calculator.java
@@ -0,0 +1,25 @@
+package org.example.p1;
+
+
+public class Calculator {
+	
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public float division(int a, int b) {
+		return a / b;
+	}
+
+	public static void main (String args[]) {
+		add(7,7);
+	}
+}
---
+++ OneRenameForOneCaller/CodeDestiny/Calculator/src/main/java/org/example/p1/Calculator.java
@@ -0,0 +1,25 @@
+package org.example.p1;
+
+
+public class Calculator {
+	
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public float division(int a, int b) {
+		return a / b;
+	}
+
+	public static void main (String args[]) {
+		sum(5,7);
+	}
+}
